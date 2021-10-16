cc1411084426aab4c640f1169fb8da1e40f7a370
Yusuf Ganiyu <airscholar@gmail.com>
2021-10-16 13:08:09 +0100

    Update dot env sanitization

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -5,6 +5,10 @@ const env = cleanEnv(process.env, {
   MONGO_URI: str({ default: "mongodb://localhost:27017/StoryBook" }),
   NODE_ENV: str({ default: "development" }),
   LOG_DIR: str({ default: "logs" }),
+  GOOGLE_CLIENT_ID: str({ default: "abc" }),
+  GOOGLE_CLIENT_SECRET: str({ default: "abc" }),
+  GOOGLE_CALLBACK_URL: str({ default: "abc" }),
+  JWT_SECRET: str({ default: "abc" }),
 });
 
 export default env;
