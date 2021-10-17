e8c11be44e612f8d409d5b6892959081b4c2737b
Yusuf Ganiyu <airscholar@gmail.com>
2021-10-17 09:12:32 +0100

    Updated middelware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,15 +1,21 @@
+// import { UserInterface } from "./../models/User.model";
 import { NextFunction, Request, Response } from "express";
+// import UserModel from "../models/User.model";
 
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
   if (!req.isAuthenticated()) {
     return res.redirect("/");
   }
+
+  // req.user = new UserModel(req.user);
   next();
 };
 
 export const guestAuthenticate = (req: Request, res: Response, next: NextFunction) => {
   if (req.isAuthenticated()) {
+    // req.user = new UserModel(req.user);
     return res.redirect("/dashboard");
   }
+
   next();
 };
