a93083ec0b6be319f403207a1360596f7f1f16dc
Yusuf Ganiyu <airscholar@gmail.com>
2021-10-17 09:09:29 +0100

    Added story model and add partial

---
+++ src/models/Story.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const storySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "public",
+    enum: ["public", "private"],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Story", storySchema);
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,11 +1,39 @@
 import mongoose from "mongoose";
 
+export interface UserInterface {
+  googleId: string;
+  email: string;
+  display_name: string;
+  first_name: string;
+  last_name: string;
+  image: string;
+  created_at: Date;
+}
+
 const userSchema = new mongoose.Schema({
-  googleId: { type: String, required: true },
-  display_name: { type: String, required: true },
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  image: { type: String },
+  googleId: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  display_name: {
+    type: String,
+    required: true,
+  },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
   created_at: {
     type: Date,
     default: Date.now,
